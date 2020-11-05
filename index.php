cd124db216686db9fff3d337d8fe64fec23f551b
felipe <felipe.carreno@hotmail.com>
2020-11-05 15:19:09 -0300

    REalizacion hasta controller

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Links</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col l4 m4 s12">
+                
+            </div>
+            <div class="col l4 m4 s12">
+                <h3 class="center"> App Links</h3>
+                <h5 class="center">Guarda tus páginas web</h5>
+            </div>
+        </div>
+    </div>
+
+<form action="#" method="POST">
+    <div class="input-field">
+        <input id="email" type="email" name="email">
+        <label for="email">Ingrese su email</label>
+    </div>
+    <div class="input-field">
+        <input id="clave" type="password" name="clave">
+        <label for="clave">Ingrese su clave</label>
+    </div>
+    <button class="btn black boton">Iniciar sesion</button>
+    <p class="center">
+        <a href="registrar.php">¿Aún no tienes cuenta?, Registrate aquí</a>
+    </p>    
+
+</form>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
